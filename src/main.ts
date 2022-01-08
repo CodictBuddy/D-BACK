1799efe220c6f6df17c9bedb9277636e627300c9
raunak-io <fullstackerr@gmail.com>
2022-01-08 23:08:48 +0530

    added required softwares in package json

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,47 @@
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
-
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+import * as mongoSanitize from 'express-mongo-sanitize';
+import * as xss from 'xss-clean';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  // app.setGlobalPrefix(process.env.API_PREFIX);
-  app.enableCors({
-    origin: '*',
-  });
+  app.use(helmet());
+  // app.use('*', cors());
+  app.enableCors({ origin: '*' });
+  app.use(cookieParser());
 
+  app.use(mongoSanitize());
+
+  app.use(xss());
+
+  app.use(compression());
   app.setGlobalPrefix(process.env.API_PREFIX);
 
   app.use('/images', express.static(join(__dirname, '..', 'images')));
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('Application API Documentation')
+    .setTitle('API Documentation')
     .setDescription('post and put request on file upload')
     .setVersion('1.0')
     .build();
 
   const doc = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('', app, doc);
-
   await app.listen(port || 3000).then(() => {
     console.log(`====>App started in port ${port}<====`);
   });
+  // await app.listen(port);
 }
 bootstrap();
