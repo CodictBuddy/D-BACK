74c7f288c63633b6d90f6d4063709b1a2c40e958
raunak-io <fullstackerr@gmail.com>
2022-01-08 18:55:31 +0530

    starting d talks backen d app

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import * as bodyParser from 'body-parser';
+
+const port = process.env.PORT || 3000;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  // app.setGlobalPrefix(process.env.API_PREFIX);
+  app.enableCors({
+    origin: '*',
+  });
+
+  const options = new DocumentBuilder()
+    .setTitle('API DOCUMENTATION')
+    .setDescription('Swagger demo API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
+  await app.listen(3000).then(() => {
+    console.log(`====>App started in port ${port}<====`);
+  });
+}
+bootstrap();
