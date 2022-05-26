787bb265b1c4e3beec656e42859e053f3e8215ac
raunak-io <fullstackerr@gmail.com>
2022-05-26 21:49:19 +0530

    removal population in notification

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -77,26 +77,26 @@ export class NotificationService {
       .find(fo)
       .sort({ created_at: -1 })
       .skip(body.skip || 0)
-      .limit(body.limit || 10)
-      .populate({
-        path: 'target_user_id',
-        select: {
-          url: 1,
-          type: 1,
-          first_name: 1,
-          last_name: 1,
-          user_headline: 1,
-          user_profile_image: 1,
-        },
-        populate: {
-          path: 'user_profile_image',
-          model: 'media',
-          select: {
-            url: 1,
-          },
-        },
-      })
-      .select('-organization_code');
+      .limit(body.limit || 10);
+    // .populate({
+    //   path: 'target_user_id',
+    //   select: {
+    //     url: 1,
+    //     type: 1,
+    //     first_name: 1,
+    //     last_name: 1,
+    //     user_headline: 1,
+    //     user_profile_image: 1,
+    //   },
+    //   populate: {
+    //     path: 'user_profile_image',
+    //     model: 'media',
+    //     select: {
+    //       url: 1,
+    //     },
+    //   },
+    // })
+    // .select('-organization_code');
     return { notifications, count: notifications.length };
   }
 
@@ -104,6 +104,8 @@ export class NotificationService {
     const fo = this.sservice.processfetchMyRecordsCondition(
       organization_code,
       token.id,
+      null,
+      'mine',
     );
     const notifications = await this.notificationModel.find({
       ...fo,
