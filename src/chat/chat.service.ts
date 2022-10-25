8ae8471ac430f1f45aaa28cd2211546099aff967
raunak-io <fullstackerr@gmail.com>
2022-10-25 22:41:22 +0530

    find query for chat room

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -83,19 +83,19 @@ export class ChatService {
       let chat_data = {};
       let room_data = await this.cRoomModel
         .findOne({
-          // $and: [
-          //   {
-          organization_code,
-          room_cat: { $eq: 'individual' },
-          // members: { $all: [conv_obj_id(token.id), conv_obj_id(body.user_id)] }
-          // members: [token.id,body.user_id] 
-          // },
-          // {
-          $or: [{ members: [token.id, body.user_id] },
-          { members: [body.user_id, token.id] }]
-          // },
-          //   { members: { $all: [conv_obj_id(token.id), conv_obj_id(body.user_id)] } },
-          // ],
+          $and: [
+            {
+              organization_code,
+              room_cat: { $eq: 'individual' },
+              // members: { $all: [conv_obj_id(token.id), conv_obj_id(body.user_id)] }
+              // members: [token.id,body.user_id] 
+            },
+            {
+              $or: [{ members: [token.id, body.user_id] },
+              { members: [body.user_id, token.id] }]
+            },
+            //   { members: { $all: [conv_obj_id(token.id), conv_obj_id(body.user_id)] } },
+          ],
         })
         .populate({
           path: 'members',
